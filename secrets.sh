133d8610b585f0891644523e73da60cc3d7f29b7
vagrant <vagrant@vagrant.vm>
2017-09-12 17:46:03 +0000

    complete markov twitter

---
+++ secrets.sh
@@ -0,0 +1,8 @@
+# Secrets for your Twitter app
+#
+# This file should never go into github!
+
+export TWITTER_CONSUMER_KEY="your-consumer-key"
+export TWITTER_CONSUMER_SECRET="your-consumer-secret"
+export TWITTER_ACCESS_TOKEN_KEY="your-access-token-key"
+export TWITTER_ACCESS_TOKEN_SECRET="your-access-token-secret"
